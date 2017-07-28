7f95d66c998068bcdd6e91a384a35840dcda2f3e
Crystal Maria <crystalcodes.0@gmail.com>
2017-07-28 01:39:47 -0400

    maps

--- routes/protected.js
+++ routes/protected.js
@@ -8,7 +8,8 @@ protectedRouter.get('/', function (req, res, next) {
     client.messages.create({
         body: '[This is a test from Safety Pin] ALERT! Crystal is in need of help',
         to: "+13142784979",
-        from: "+13143000312"
+        from: "+13143000312",
+        mediaUrl: "https://images1.apartments.com/m2/e9L4T6aIVxh00vqc0l6yxI01qe2lY1JDnBGAdvWYYIk/H240W360/broadstone-at-brickell-miami-fl-map-image-of-the-property.jpg",
     }, function (err, data) {
         if (err) {
             console.log("Could not send message");
@@ -18,62 +19,57 @@ protectedRouter.get('/', function (req, res, next) {
         }
         ;
     });
-    client.messages.create({
-        // Nik
-        body: 'This is a test from Safety Pin] ALERT! Crystal is in need of help',
-        to: "+17039018334",
-        from: "+13143000312"
-    }, function (err, data) {
-        if (err) {
-            console.log("Could not send message");
-        }
-        else {
-            console.log("Has been contacted");
-        }
-        ;
-    });
-    client.messages.create({
-        // Blake
-        body: 'This is a test from Safety Pin] ALERT! Crystal is in need of help',
-        to: "+16784711521",
-        from: "+13143000312"
-    }, function (err, data) {
-        if (err) {
-            console.log("Could not send message");
-        }
-        else {
-            console.log("Has been contacted");
-        }
-        ;
-    });
-    client.messages.create({
-        // Haney
-        body: 'This is a test from Safety Pin] ALERT! Crystal is in need of help',
-        to: "+13136106987",
-        from: "+13143000312"
-    }, function (err, data) {
-        if (err) {
-            console.log("Could not send message");
-        }
-        else {
-            console.log("Has been contacted");
-        }
-        ;
-    });
-    client.messages.create({
-        // Edward
-        body: 'This is a test from Safety Pin] ALERT! Crystal is in need of help',
-        to: "+13124021551",
-        from: "+13143000312"
-    }, function (err, data) {
-        if (err) {
-            console.log("Could not send message");
-        }
-        else {
-            console.log("Has been contacted");
-        }
-        ;
-    });
+    // client.messages.create({
+    //   // Nik
+    //     body: '[This is a test from Safety Pin] ALERT! Crystal is in need of help',
+    //     to: "+17039018334",
+    //     from: "+13143000312"
+    // }, function(err, data){
+    //    if(err) {
+    //        console.log("Could not send message");
+    //    }else{
+    //        console.log("Has been contacted");
+    //    };
+    // });
+    //
+    // client.messages.create({
+    //   // Blake
+    //     body: '[This is a test from Safety Pin] ALERT! Crystal is in need of help',
+    //     to: "+16784711521",
+    //     from: "+13143000312"
+    // }, function(err, data){
+    //    if(err) {
+    //        console.log("Could not send message");
+    //    }else{
+    //        console.log("Has been contacted");
+    //    };
+    // });
+    //
+    // client.messages.create({
+    //   // Haney
+    //     body: '[This is a test from Safety Pin] ALERT! Crystal is in need of help',
+    //     to: "+13136106987",
+    //     from: "+13143000312"
+    // }, function(err, data){
+    //    if(err) {
+    //        console.log("Could not send message");
+    //    }else{
+    //        console.log("Has been contacted");
+    //    };
+    // });
+    //
+    // client.messages.create({
+    //   // Edward
+    //     body: '[This is a test from Safety Pin] ALERT! Crystal is in need of help',
+    //     to: "+13124021551",
+    //     from: "+13143000312"
+    // }, function(err, data){
+    //    if(err) {
+    //        console.log("Could not send message");
+    //    }else{
+    //        console.log("Has been contacted");
+    //    };
+    // });
     var fakeObject = {
         message: "This is the message you got from the backend"
     };
